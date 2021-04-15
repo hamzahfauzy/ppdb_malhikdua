c5394adfc2fa229e73bf795b8a08f48622720fe3
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-04-15 21:14:25 +0700

    add no kk in staff

--- resources/views/staff/siswa/show.blade.php
+++ resources/views/staff/siswa/show.blade.php
@@ -89,6 +89,10 @@
                     </div>
                     <div class="col-12 col-md-6">
                         <div class="form-group">
+                            <label for="">No. KK</label>
+                            <p>{{$formulir->data_diri->no_kk}}</p>
+                        </div>
+                        <div class="form-group">
                             <label for="">NIK</label>
                             <p>{{$formulir->data_diri->NIK}}</p>
                         </div>
